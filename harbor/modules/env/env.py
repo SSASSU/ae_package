ca295b6cac9e9e0a46a35ff7af9eb5dfe060d2ee
SSASSU <pururoong@gmail.com>
2023-09-20 08:41:24 +0000

    config함수이름 변경, node ip 설정 기능 개발중

--- harbor/modules/env/env.py
+++ harbor/modules/env/env.py
@@ -1,7 +1,9 @@
 import subprocess
 import os 
 
-def config():
+from kubernetes import client, config
+
+def ssl_config():
     command = 'openssl version'
 
     try:
@@ -11,3 +13,22 @@ def config():
 
     except subprocess.CalledProcessError as e:
         print("error:", e)
+
+
+def ssh_key_copy():
+
+    config.load_kube_config()
+
+    #Get Node Info. 
+    v1 = client.CoreV1Api()
+    node_list = v1.list_node().items
+
+    #Get Internal Node IP
+    node_ips = []
+    for node in node_list:
+        addresses = node.status.addresses
+        for addr in addresses:
+            if addr.type == "InternalIP":
+                node_ips.append(addr.address)
+
+    print (node_ips)
